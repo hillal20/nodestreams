ca44a38b163e13b926e21707fcd825c0af1e24c6
hilal aissani <hilalaissani@gmail.com>
2019-04-21 20:02:10 -0400

    stream

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ const server = require("http").createServer();
 
 /// reading file
 
-// server.on("request", (req, res) => {
-//   fs.readFile("./big.file", (err, data) => {
-//     if (err) {
-//       res.json({ err: err });
-//     }
-//     res.end(data);
-//   });
-// });
-
-// reading stream
 server.on("request", (req, res) => {
-  const src = fs.createReadStream("./big.file");
-  src.pipe(res);
+  fs.readFile("./big.file", (err, data) => {
+    if (err) {
+      res.json({ err: err });
+    }
+    res.json({ data: data });
+  });
 });
+
+//////////////   reading stream
+// server.on("request", (req, res) => {
+//   const src = fs.createReadStream("./big.file");
+//   src.pipe(res);
+// });
 
 server.listen(6000, () => {
   console.log("===> server on port 6000");
